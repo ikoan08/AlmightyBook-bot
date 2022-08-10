b172bae8131aab512dd3ccc3d5fcc91a1d03d2e7
NaokiWaka08 <wnp1998@gmail.com>
2022-08-10 19:41:10 +0900

    失敗機能

--- modules/heart.py
+++ modules/heart.py
@@ -8,12 +8,8 @@ is_accept = True
 # 画像を追加する場合はGIMPで縦を150にそろえる
 async def func(message):
     global is_accept
-    if str.isdecimal(message.content[7:]):
+    if str.isdecimal(message.content[7:]) and int(message.content[7:]) > 0 and int(message.content[7:]) < 4:
         num = int(message.content[7:])
-        if num < 0 or num > 3:
-            await message.channel.send("不正な数です")
-            return
-
         if not is_accept:
             await message.channel.send("他の人が心臓抽選中です")
             return
@@ -23,3 +19,5 @@ async def func(message):
                 file_name = f'hearts/heart{i}.jpg'
                 await message.channel.send(file=discord.File(file_name))
         is_accept = True
+    else:
+        await message.channel.send(file=discord.File("hearts/not_heart.jpg"))
