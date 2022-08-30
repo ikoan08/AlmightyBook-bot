2c85652925abbe95f6140c7203d3dccc62a97042
Naoki <36870844+Naoki08@users.noreply.github.com>
2022-08-31 08:43:13 +0900

    心臓画像追加

--- modules/heart.py
+++ modules/heart.py
@@ -15,7 +15,7 @@ async def func(message):
             return
 
         is_accept = False
-        for i in random.sample(range(1, 19), k=num):
+        for i in random.sample(range(1, 28), k=num):
                 file_name = f'hearts/heart{i}.jpg'
                 await message.channel.send(file=discord.File(file_name))
         is_accept = True
